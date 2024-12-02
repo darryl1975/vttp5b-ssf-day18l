fbba3e2ee510a17b20bf6022f312e63715497bd7
darryl1975 <darryl1975@hotmail.com>
2024-12-02 09:46:32 +0800

    initial commit

---
+++ src/test/java/sg/edu/nus/iss/vttp5b_ssf_day18l/Vttp5bSsfDay18lApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5b_ssf_day18l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5bSsfDay18lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
